291a8ee618ffa8226f04476cc2f0a22ab6ce2c61
sigma <dota22papich@mail.ru>
2025-10-18 14:33:34 +0300

    fix mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,20 @@
 export default {
     ssr: true,
     target: 'static',
+    head: {
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ]
+    },
     css: ['@/assets/scss/main.scss'],
     plugins: ['@/plugins/api.js'],
     modules: ['@nuxtjs/vuetify'],
     vuetify: {
         customVariables: ['~/assets/scss/_variables.scss'],
+        breakpoint: {
+            mobileBreakpoint: 'sm'
+        }
     },
     build: {
         transpile: [/^vuetify/],
